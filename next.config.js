912f96dae65da643d0f6d4aa22ec12d35a5f397b
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-07-22 11:51:41 +0300

    well-known

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/.well-known/:path*",
-        destination: "/.well-known/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
