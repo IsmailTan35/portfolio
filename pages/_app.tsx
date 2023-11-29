3bd28ebaf15e7e8ab6dc9faa9f9d44e2a5f5ed52
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-11-29 19:32:31 +0300

    Add react-hotjar package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import "../styles/globals.css";
-
+import { hotjar } from "react-hotjar";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3763776, 6);
+  }, []);
   return <Component {...pageProps} />;
 }
 
